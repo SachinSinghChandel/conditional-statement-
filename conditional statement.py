c8b7da16b1b8d9d62e2e520378a2b5967d1db834
SachinSinghChandel <sachinsinghbhp@gmail.com>
2025-09-20 20:38:13 +0530

    added conditional statement.py

---
+++ conditional statement.py
@@ -0,0 +1,28 @@
+#wap is eligible  for voting
+# age=int(input("Your age is:"))
+# print(age)
+# if(age>=18):
+#     print('you are  eligible for voting ')
+# else:
+#     print("you are not eligible for voting")    
+
+# Write a program of traffic light
+# light=input("light is")
+# print(light)
+# if(light=="red"):
+#     print("stop")
+# elif(light=="green"):
+#     print("go")
+# else:
+#     print("wait")       
+    
+# Nesting 
+age=int(input("Your age is:"))
+print(age)
+if(age>=18):
+    if(age>=80):
+        print("you can't drive")
+    else:
+        print("you can drive")    
+else:
+    print("can't drive")        
